7821f9591a9c693e5d13ca387f173bb6c359e4c7
Caruychen <caruychen@gmail.com>
2021-02-17 15:49:45 +0200

    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const nodemon = require('nodemon')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://phonebook_user:${password}@cluster0.wnchk.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length > 3) {
+  const name = process.argv[3]
+  const number = process.argv[4] || (console.log('number missing'), process.exit(1))
+  const person = new Person({ name, number })
+
+  person.save().then(result => {
+    console.log('added', result.name, 'number', result.number, 'to phonebook')
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => console.log(person.name, person.number))
+    mongoose.connection.close()
+  })
+}
+
+
